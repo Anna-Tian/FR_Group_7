0db307d300581cd04673753960fd71c410fb0d4d
Cheryl711 <ahorukawa711@163.com>
2018-05-23 23:43:36 +1000

    recommendation founction fixed

--- app/src/main/java/com/example/anna/fr/home/HomeActivity.java
+++ app/src/main/java/com/example/anna/fr/home/HomeActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 
 import android.support.v7.widget.CardView;
 
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 
 import android.support.v7.widget.RecyclerView;
@@ -50,10 +51,13 @@ import com.example.anna.fr.utils.FilterActivity;
 
 import com.example.anna.fr.utils.RestaurantActivity;
 
+import com.example.anna.fr.utils.SearchAdapter;
 import com.example.anna.fr.utils.UniversalImageLoader;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 
 import com.google.firebase.database.DatabaseError;
@@ -68,6 +72,7 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -96,6 +101,14 @@ public class HomeActivity extends AppCompatActivity {
 
     DatabaseReference databaseReference2;
 
+    RecyclerView recyclerView;
+    FirebaseUser firebaseUser;
+    ArrayList<String> nameList;
+    ArrayList<String> addressList;
+    ArrayList<String> profile_photoList;
+    SearchAdapter searchAdapter;
+
+
 
 
 
@@ -122,9 +135,6 @@ public class HomeActivity extends AppCompatActivity {
 
         myRef.keepSynced(true);
 
-        mRef = FirebaseDatabase.getInstance().getReference().child(mContext.getString(R.string.dbname_restaurant_details));
-
-        mRef.keepSynced(true);
 
 
 
@@ -144,268 +154,229 @@ public class HomeActivity extends AppCompatActivity {
 
 
 
+        recyclerView = (RecyclerView) findViewById( R.id.recyclerView );
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        recyclerView.setHasFixedSize( true );
+        recyclerView.setLayoutManager( new LinearLayoutManager( this ) );
+        recyclerView.addItemDecoration( new DividerItemDecoration( this, LinearLayoutManager.VERTICAL ) );
+
+        nameList = new ArrayList<>();
+        addressList = new ArrayList<>();
+        profile_photoList = new ArrayList<>();
+
+        setAdapter();
 
 
-//        Collections.sort(mRestaurantIntro, RestaurantIntro.ComparatorBy);
-
-//        Collections.sort(mRestaurantDetails, RestaurantDetails.ComparatorBy);
 
     }
 
 
 
-    @Override
 
-    protected void onStart() {
+        private void setAdapter() {
 
-        super.onStart();
-
-        FirebaseRecyclerAdapter<RestaurantDetails, ResViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<RestaurantDetails, ResViewHolder>
-
-                (RestaurantDetails.class, R.layout.snippet_center_restaurant_introduction, ResViewHolder.class, mRef) {
-
-            @Override
-
-            protected void populateViewHolder(ResViewHolder viewHolder, final RestaurantDetails model, int position) {
-
-                viewHolder.setName(model.getName());
-
-                viewHolder.setProfile_photo(getApplicationContext(),model.getProfile_photo());
-
-                viewHolder.setAddress(model.getAddress());
-
-                databaseReference.child("restaurant_details").addListenerForSingleValueEvent(new ValueEventListener() {
-
-                    @Override
-
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-
-                        for (final DataSnapshot ds: dataSnapshot.getChildren()){
-
-                            String name = ds.child("name").getValue(String.class);
-
-                            String address = ds.child("address").getValue(String.class);
-
-                            if(areSame(model.getName(),name)&&areSame(model.getAddress(),address)){
-
-                                databaseReference2.child("restaurant_comments").child(ds.child("res_id").getValue(String.class)).addListenerForSingleValueEvent(new ValueEventListener() {
-
-                                    @Override
-
-                                    public void onDataChange(DataSnapshot dataSnapshot) {
-
-                                        int sum = 0;
-
-                                        int count = 0;
-
-                                        for (DataSnapshot ds2 : dataSnapshot.getChildren()){
-
-                                            sum += ds2.child("rating").getValue(float.class).intValue();
-
-                                            count++;
-
-                                        }
-
-                                        Log.d(TAG, "onDataChange: ++++"+sum+" / "+count+" = "+ sum/count);
-
-                                        databaseReference.child("restaurant_details").child(ds.child("res_id").getValue(String.class)).child("rating").setValue(sum/count);
-
-                                    }
+            databaseReference.child("restaurant_details").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    nameList.clear();
+                    addressList.clear();
+                    profile_photoList.clear();
+                    recyclerView.removeAllViews();
 
 
+                    for(int counter=5 ;counter>0;counter-- ){
+                        for (final DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            String uid = snapshot.getKey();
+                            String name = snapshot.child("name").getValue(String.class);
+                            String address = snapshot.child("address").getValue(String.class);
+                            String profile_photo = snapshot.child("profile_photo").getValue(String.class);
+                            float rating = snapshot.child("rating").getValue(float.class);
 
-                                    @Override
+//                            databaseReference2.child("restaurant_comments");
+//                            databaseReference2.child(snapshot.child("res_id").getValue(String.class));
+//                            databaseReference2.addListenerForSingleValueEvent(new ValueEventListener() {
+//
+//                                @Override
+//
+//                                public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                                    int sum = 0;
+//
+//                                    int count = 0;
+//
+//                                    for (DataSnapshot ds2 : dataSnapshot.getChildren()) {
+//
+//                                        sum += ds2.child("rating").getValue(float.class).intValue();
+//
+//                                        count++;
+//
+//                                    }
+//
+//                                    Log.d(TAG, "onDataChange: ++++" + sum + " / " + count + " = " + sum / count);
+//
+//                                    databaseReference.child("restaurant_details").child(snapshot.child("res_id").getValue(String.class)).child("rating").setValue(sum / count);
+//
 
-                                    public void onCancelled(DatabaseError databaseError) {
 
-
-
-                                    }
-
-                                });
-
-                                Log.d(TAG, "onDataChange: rating bar ++++++");
+                            if (rating == counter) {
+                                nameList.add(name);
+                                addressList.add(address);
+                                profile_photoList.add(profile_photo);
 
                             }
 
                         }
-
                     }
+                    searchAdapter = new SearchAdapter(com.example.anna.fr.home.HomeActivity.this, nameList, addressList, profile_photoList);
+                    recyclerView.setAdapter(searchAdapter);
+
+                }
+
+
+                           @Override
+                           public void onCancelled(DatabaseError databaseError) {
+
+                                                       }
+                       });
+                                    Log.d(TAG, "onDataChange: rating bar ++++++");
+                                       }
 
 
 
-                    @Override
-
-                    public void onCancelled(DatabaseError databaseError) {
 
 
 
-                    }
+        private void initImageLoader(){
+            UniversalImageLoader universalImageLoader = new UniversalImageLoader(mContext);
+            ImageLoader.getInstance().init(universalImageLoader.getConfig());
+        }
+        private void setupToolbar(){
+            Toolbar toolbar = (Toolbar) findViewById(R.id.homeBar);
+            setSupportActionBar(toolbar);
 
-                });
+            ImageView filterMenu = (ImageView) findViewById(R.id.filterMenu);
+            filterMenu.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "onClick: navigating to filter selection.");
+                    Intent intent = new Intent(mContext, FilterActivity.class);
+                    startActivity(intent);
 
-                viewHolder.setRating((int) model.getRating());
-
-
-
-                viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
-
-                    @Override
-
-                    public void onClick(View view) {
-
-                        Toast.makeText(mContext, model.getName(), Toast.LENGTH_SHORT).show();
-
-
-
-                        Intent intent = new Intent(mContext,RestaurantActivity.class);
-
-                        intent.putExtra("name",model.getName());
-
-                        intent.putExtra("profilePhoto",model.getProfile_photo());
-
-                        intent.putExtra("address",model.getAddress());
-
-                        intent.putExtra("phone",model.getPhone());
-
-                        intent.putExtra("rating",model.getRating());
-
-
-
-                        startActivity(intent);
-
-                    }
-
-                });
-
-            }
-
-        };
-
-        mResList.setAdapter(firebaseRecyclerAdapter);
-
-    }
-
-
-
-    public static class ResViewHolder extends RecyclerView.ViewHolder{
-
-        View mView;
-
-        public ResViewHolder(View itemView){
-
-            super(itemView);
-
-            mView = itemView;
+                }
+            });
 
         }
 
-        public void setName(String name){
 
-            TextView res_name = (TextView) mView.findViewById(R.id.restaurantName);
-
-            res_name.setText(name);
-
-        }
-
-        public void setProfile_photo(Context ctx,String profile_photo){
-
-            ImageView res_image = (ImageView) mView.findViewById(R.id.restaurantImage);
-
-            Picasso.with(ctx).load(profile_photo).into(res_image);
-
-        }
-
-        public void setAddress(String address){
-
-            TextView res_address = (TextView) mView.findViewById(R.id.restaurantAddress);
-
-            res_address.setText(address);
+        // BottomNavigationView setup
+        private void setupBottomNavigationView() {
+            Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+            BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavViewBar);
+            BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationView);
+            Menu menu = bottomNavigationView.getMenu();
+            MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+            menuItem.setChecked(true);
 
         }
 
 
 
-        public void setRating(int rating){
-
-
-
-            TextView ratingBarT = (TextView) mView.findViewById(R.id.ratingBarText);
-
-            ratingBarT.setText(rating+"");
-
-            RatingBar ratingBar = (RatingBar) mView.findViewById(R.id.ratingBar);
-
-            ratingBar.setRating(rating);
-
-        }
-
-    }
-
-
-
-    private void initImageLoader(){
-
-        UniversalImageLoader universalImageLoader = new UniversalImageLoader(mContext);
-
-        ImageLoader.getInstance().init(universalImageLoader.getConfig());
-
-    }
-
-    private void setupToolbar(){
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.homeBar);
-
-        setSupportActionBar(toolbar);
-
-
-
-        ImageView filterMenu = (ImageView) findViewById(R.id.filterMenu);
-
-        filterMenu.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-
-            public void onClick(View v) {
-
-                Log.d(TAG, "onClick: navigating to filter selection.");
-
-                Intent intent = new Intent(mContext, FilterActivity.class);
-
-                startActivity(intent);
-
-
-
-            }
-
-        });
-
-
-
-    }
-
-
-
-    // BottomNavigationView setup
-
-    private void setupBottomNavigationView() {
-
-        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
-
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavViewBar);
-
-        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationView);
-
-        Menu menu = bottomNavigationView.getMenu();
-
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-
-        menuItem.setChecked(true);
-
-
-
-    }
-
+//
+//
+//    @Override
+//
+//    protected void onStart() {
+//
+//        super.onStart();
+//
+//        FirebaseRecyclerAdapter<RestaurantDetails, ResViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<RestaurantDetails, ResViewHolder>
+//
+//                (RestaurantDetails.class, R.layout.snippet_center_restaurant_introduction, ResViewHolder.class, mRef) {
+//
+//            @Override
+//
+//            protected void populateViewHolder(ResViewHolder viewHolder, final RestaurantDetails model, int position) {
+//
+//                viewHolder.setName(model.getName());
+//
+//                viewHolder.setProfile_photo(getApplicationContext(),model.getProfile_photo());
+//
+//                viewHolder.setAddress(model.getAddress());
+//
+//                databaseReference.child("restaurant_details").addListenerForSingleValueEvent(new ValueEventListener() {
+//
+//                    @Override
+//
+//                    public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                        for (final DataSnapshot ds: dataSnapshot.getChildren()){
+//
+//                            String name = ds.child("name").getValue(String.class);
+//
+//                            String address = ds.child("address").getValue(String.class);
+//
+//                            if(areSame(model.getName(),name)&&areSame(model.getAddress(),address)){
+//
+//                                databaseReference2.child("restaurant_comments").child(ds.child("res_id").getValue(String.class)).addListenerForSingleValueEvent(new ValueEventListener() {
+//
+//                                    @Override
+//
+//                                    public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                                        int sum = 0;
+//
+//                                        int count = 0;
+//
+//                                        for (DataSnapshot ds2 : dataSnapshot.getChildren()){
+//
+//                                            sum += ds2.child("rating").getValue(float.class).intValue();
+//
+//                                            count++;
+//
+//                                        }
+//
+//                                        Log.d(TAG, "onDataChange: ++++"+sum+" / "+count+" = "+ sum/count);
+//
+//                                        databaseReference.child("restaurant_details").child(ds.child("res_id").getValue(String.class)).child("rating").setValue(sum/count);
+//
+//                                    }
+//
+//
+//
+//                                    @Override
+//
+//                                    public void onCancelled(DatabaseError databaseError) {
+//
+//
+//
+//                                    }
+//
+//                                });
+//
+//                                Log.d(TAG, "onDataChange: rating bar ++++++");
+//
+//                            }
+//
+//                        }
+//
+//                    }
+//
+//
+//
+//                    @Override
+//
+//                    public void onCancelled(DatabaseError databaseError) {
+//
+//
+//
+//                    }
+//
+//                });
+//
+//
 
 
     private boolean areSame(String string1, String string2){
